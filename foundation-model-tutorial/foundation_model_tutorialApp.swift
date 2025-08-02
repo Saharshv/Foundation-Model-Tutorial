eac929d09a379eef334c461513f7b94baa449367
Saharsh Vedi <saharsh@magicx.ai>
2025-08-02 16:02:25 -0700

    Initial Commit

---
+++ foundation-model-tutorial/foundation_model_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  foundation_model_tutorialApp.swift
+//  foundation-model-tutorial
+//
+//  Created by Saharsh Vedi on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct foundation_model_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
